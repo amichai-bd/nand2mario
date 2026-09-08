31d3b9c193c4294f3d629c93a10410058fb8b62a
amichaibd <amichaibd@gmail.com>
2026-09-08 20:14:06 +0300

    Connect delivered DMA arbitration to v05 system

--- src/rtl/system/n2m_v05_system.sv
+++ src/rtl/system/n2m_v05_system.sv
@@ -34,7 +34,6 @@ module n2m_v05_system #(
     output logic fault
 );
     // Quartus 25.1 misresolves package constants inside instance connections.
-    localparam n2m_memory_pkg::memory_destination_t OAM_DESTINATION = n2m_memory_pkg::MEMORY_OAM;
     localparam n2m_memory_pkg::memory_destination_t IRQ_DESTINATION = n2m_memory_pkg::MEMORY_IRQ;
     localparam n2m_memory_pkg::memory_destination_t PPU_DESTINATION = n2m_memory_pkg::MEMORY_PPU;
     localparam n2m_memory_pkg::memory_destination_t JOYP_DESTINATION = n2m_memory_pkg::MEMORY_JOYP;
@@ -56,21 +55,29 @@ module n2m_v05_system #(
     logic joyp_selected_active, joyp_event;
     logic [12:0] frame_address;
     logic request_valid, response_valid, cpu_initialized, cpu_fault, memory_fault, ppu_fault;
-    logic memory_initialized, storage_read, storage_write;
-    n2m_memory_pkg::memory_store_t storage_store, raw_store;
+    logic memory_initialized, storage_valid;
+    n2m_memory_pkg::memory_store_t raw_store;
     n2m_memory_pkg::memory_destination_t destination;
-    logic [14:0] storage_offset, raw_offset;
-    logic [7:0] storage_wdata, storage_rdata;
-    logic storage_valid, owner_prepare, owner_commit, owner_write;
+    logic [14:0] raw_offset;
+    logic [7:0] raw_wdata, storage_rdata;
+    logic owner_prepare, owner_commit, owner_write;
     logic [15:0] owner_address;
     logic [7:0] owner_wdata, owner_rdata;
     logic owner_valid, owner_service;
-    logic raw_read, raw_write, video_owner, video_allowed, video_read;
-    logic oam_cpu_late_write, late_busy, late_fault, oam_read_allowed;
-    n2m_memory_pkg::memory_oam_request_t late_request;
-    n2m_memory_pkg::memory_oam_response_t late_response;
-    logic video_pending;
-    logic [15:0] video_address;
+    logic raw_read, raw_write, dma_active, cpu_halted;
+    logic dma_vram_allow, dma_oam_allow, oam_cpu_late_write, oam_late_future;
+    n2m_memory_pkg::memory_oam_request_t oam_request;
+    n2m_memory_pkg::memory_oam_response_t oam_response;
+    n2m_cpu_pkg::cpu_bus_plan_t bus_plan;
+    n2m_cpu_pkg::cpu_address_effect_t address_effect;
+    logic address_effect_resolved, address_effect_sample;
+    logic [1:0] cpu_phase;
+    logic [5:0] oam_scan_index;
+    logic raw_vram_read, raw_vram_valid, raw_oam_read, raw_oam_valid;
+    logic [12:0] raw_vram_address;
+    logic [7:0] raw_vram_data;
+    logic [6:0] raw_oam_pair;
+    logic [15:0] raw_oam_data;
     logic [7:0] ppu_rdata, irq_rdata;
     logic ppu_selected, irq_selected;
     logic [7:0] ie_stored, ie_observe;
@@ -90,7 +97,7 @@ module n2m_v05_system #(
     logic [63:0] observe_sequence, observe_dot;
     assign reset = reset_sys || core_reset;
     assign core_initialized = memory_initialized && cpu_initialized;
-    assign fault = cpu_fault || memory_fault || ppu_fault || late_fault;
+    assign fault = cpu_fault || memory_fault || ppu_fault;
 
     n2m_uart #(.CLOCK_HZ(25000000), .BAUD(UART_BAUD)) u_uart (
         .clk_sys, .reset_sys, .uart_rx, .uart_tx,
@@ -112,45 +119,49 @@ module n2m_v05_system #(
         .buttons(effective_buttons),
         .read_data, .response_valid, .joyp_selected_active, .wake_request(1'b0),
         .request_valid, .address, .write_data, .write_enable, .bus_commit, .irq_ack,
-        .access_kind(), .address_effect(), .address_effect_resolved(),
-        .address_effect_sample(), .address_effect_phase(), .halted(), .stopped(cpu_stopped),
+        .access_kind(bus_plan.access_kind), .address_effect, .address_effect_resolved,
+        .address_effect_sample, .address_effect_phase(cpu_phase), .halted(cpu_halted), .stopped(cpu_stopped),
         .locked(), .initialized(cpu_initialized), .fault(cpu_fault), .ime_observe(),
         .ime_delay_observe(), .stop_execute(), .divider_reset_request,
         .instruction_complete, .retirement_valid, .retirement
     );
-    n2m_memory_cpu_port u_cpu_port (
-        .clk_sys, .reset_sys, .core_reset, .init_done(memory_initialized && image_valid),
-        .request_valid, .address, .write_enable, .write_data, .bus_commit,
-        .read_data, .response_valid, .contract_fault(memory_fault),
-        .storage_read, .storage_write, .storage_store, .storage_offset,
-        .storage_wdata, .storage_rdata, .storage_valid,
-        .owner_prepare, .owner_commit, .owner_destination(destination),
-        .owner_address, .owner_write, .owner_wdata, .owner_rdata, .owner_valid,
-        .owner_service_available(owner_service)
+    assign bus_plan.address = address;
+    assign bus_plan.write_data = write_data;
+    assign bus_plan.write_enable = write_enable;
+    assign dma_vram_allow = bus_plan.write_enable ? vram_cpu_allow : vram_cpu_read_allow;
+    assign dma_oam_allow = bus_plan.write_enable ? oam_cpu_allow : oam_cpu_read_allow;
+    // CPU faults are registered. Stop subsequent work without feeding a missing
+    // response back into the arbiter on the edge which discovers that fault.
+    n2m_dma u_dma (
+        .clk_sys, .reset_sys, .core_reset,
+        .init_done(memory_initialized && image_valid && !cpu_fault),
+        .gb_tick(gb_tick && !cpu_fault), .cpu_phase, .cpu_halted, .cpu_stopped,
+        .request_valid(request_valid && !cpu_fault), .bus_plan,
+        .bus_commit(bus_commit && !cpu_fault), .address_effect,
+        .address_effect_resolved(address_effect_resolved && !cpu_fault),
+        .address_effect_sample(address_effect_sample && !cpu_fault),
+        .read_data, .response_valid, .fault(memory_fault),
+        .peripheral_prepare(owner_prepare), .peripheral_commit(owner_commit),
+        .peripheral_destination(destination), .peripheral_address(owner_address),
+        .peripheral_write(owner_write), .peripheral_wdata(owner_wdata),
+        .peripheral_rdata(owner_rdata), .peripheral_valid(owner_valid),
+        .peripheral_available(owner_service), .vram_cpu_allow(dma_vram_allow),
+        .oam_cpu_allow(dma_oam_allow), .oam_cpu_late_write, .oam_late_future,
+        .ppu_vram_request(vram_request), .ppu_vram_address(vram_address),
+        .ppu_vram_data(vram_data), .ppu_vram_valid(vram_valid),
+        .ppu_oam_phase(oam_phase), .ppu_scan_index(oam_scan_index),
+        .ppu_oam_pair(oam_pair_address), .ppu_oam_data(oam_data), .ppu_oam_valid(oam_valid),
+        .dma_active, .access_read(raw_read), .access_write(raw_write),
+        .access_store(raw_store), .access_address(raw_offset), .access_wdata(raw_wdata),
+        .access_rdata(storage_rdata), .access_valid(storage_valid), .oam_request, .oam_response,
+        .raw_vram_read, .raw_vram_address, .raw_vram_data, .raw_vram_valid,
+        .raw_oam_read, .raw_oam_pair, .raw_oam_data, .raw_oam_valid
     );
-    // One raw A port: the public CPU address can select only one destination.
-    // PPU permissions apply to CPU video accesses; its own B reads remain live.
-    assign video_owner = destination == n2m_memory_pkg::MEMORY_VRAM || destination == n2m_memory_pkg::MEMORY_OAM;
-    assign video_allowed = destination == n2m_memory_pkg::MEMORY_VRAM ? (owner_write ? vram_cpu_allow : vram_cpu_read_allow)
-        : (owner_write ? (oam_cpu_allow || oam_cpu_late_write) : oam_cpu_read_allow);
-    assign video_read = owner_prepare && video_owner && video_allowed && !owner_write
-        && !(destination == n2m_memory_pkg::MEMORY_OAM && late_busy);
-    assign raw_read = storage_read || video_read;
-    assign raw_write = storage_write || (owner_commit && video_owner && video_allowed && owner_write
-        && !(destination == n2m_memory_pkg::MEMORY_OAM && (late_busy || oam_cpu_late_write)));
-    assign raw_store = video_owner ? (destination == n2m_memory_pkg::MEMORY_VRAM ? n2m_memory_pkg::STORE_VRAM : n2m_memory_pkg::STORE_OAM) : storage_store;
-    assign raw_offset = video_owner ? (destination == n2m_memory_pkg::MEMORY_VRAM ? {2'd0,address[12:0]} : {7'd0,address[7:0]}) : storage_offset;
-    `DFF_ARST_VAL(video_pending, video_read, clk_sys, reset, 1'b0)
-    `DFF_EN(video_address, address, clk_sys, video_read)
     always_comb begin
         owner_service = 1;
         owner_valid = 1;
         owner_rdata = 0;
         case (destination)
-            n2m_memory_pkg::MEMORY_VRAM, n2m_memory_pkg::MEMORY_OAM: begin
-                owner_rdata = video_allowed ? storage_rdata : 8'hff;
-                owner_valid = !video_allowed || (video_pending && video_address == address && storage_valid);
-            end
             n2m_memory_pkg::MEMORY_TIMER: owner_rdata = timer_rdata;
             n2m_memory_pkg::MEMORY_PPU: owner_rdata = ppu_rdata;
             n2m_memory_pkg::MEMORY_IRQ: owner_rdata = irq_rdata;
@@ -162,26 +173,17 @@ module n2m_v05_system #(
             default: begin owner_service = 0; owner_valid = 0; end
         endcase
     end
-    n2m_oam_late_write u_oam_late (
-        .clk_sys, .reset_sys, .core_reset,
-        .prepare(owner_prepare && destination == OAM_DESTINATION && owner_write),
-        .commit(owner_commit && destination == OAM_DESTINATION && owner_write),
-        .late_window(oam_cpu_late_write), .address(owner_address), .data(owner_wdata),
-        .ppu_read(oam_phase != 0), .ppu_pair(oam_pair_address),
-        .response(late_response), .request(late_request), .raw_oam_busy(late_busy),
-        .late_commit(), .ppu_read_allowed(oam_read_allowed), .fault(late_fault)
-    );
-    n2m_memory_stores u_stores (.oam_request(late_request), .oam_response(late_response),
+    n2m_memory_stores u_stores (.oam_request, .oam_response,
         .clk_sys, .reset_sys, .core_reset, .init_done(memory_initialized),
         .access_read(raw_read), .access_write(raw_write), .access_store(raw_store),
-        .access_address(raw_offset), .access_wdata(write_data),
+        .access_address(raw_offset), .access_wdata(raw_wdata),
         .access_rdata(storage_rdata), .access_valid(storage_valid),
         .host_read(rom_read), .host_write(rom_write), .host_offset({17'd0,rom_address}),
         .host_wdata(rom_write_data), .host_rdata(rom_read_data), .host_valid(rom_read_valid),
-        .ppu_vram_read(vram_request), .ppu_vram_address(vram_address),
-        .ppu_vram_rdata(vram_data), .ppu_vram_valid(vram_valid),
-        .ppu_oam_read(oam_read_allowed), .ppu_oam_pair(oam_pair_address),
-        .ppu_oam_rdata(oam_data), .ppu_oam_valid(oam_valid),
+        .ppu_vram_read(raw_vram_read), .ppu_vram_address(raw_vram_address),
+        .ppu_vram_rdata(raw_vram_data), .ppu_vram_valid(raw_vram_valid),
+        .ppu_oam_read(raw_oam_read), .ppu_oam_pair(raw_oam_pair),
+        .ppu_oam_rdata(raw_oam_data), .ppu_oam_valid(raw_oam_valid),
         .wave_read(1'b0), .wave_address(4'd0), .wave_rdata(), .wave_valid()
     );
     n2m_timer u_timer (
@@ -203,8 +205,8 @@ module n2m_v05_system #(
         .io_commit(owner_commit && destination == PPU_DESTINATION), .io_write(owner_write),
         .io_address(owner_address), .io_wdata(owner_wdata), .io_selected(ppu_selected),
         .io_rdata(ppu_rdata), .vram_request, .vram_address, .vram_data, .vram_valid,
-        .oam_pair_address, .oam_phase, .oam_scan_index(), .oam_data, .oam_valid,
-        .dma_active(1'b0), .vram_cpu_allow, .oam_cpu_allow, .vram_cpu_read_allow, .oam_cpu_read_allow, .oam_late_future(), .oam_cpu_late_write, .stat_condition,
+        .oam_pair_address, .oam_phase, .oam_scan_index, .oam_data, .oam_valid,
+        .dma_active, .vram_cpu_allow, .oam_cpu_allow, .vram_cpu_read_allow, .oam_cpu_read_allow, .oam_late_future, .oam_cpu_late_write, .stat_condition,
         .vblank_condition, .stat_rise(), .vblank_rise(), .fault(ppu_fault),
         .source_valid, .source_start, .source_shade, .source_x, .source_y,
         .source_epoch, .source_dot, .source_abort, .blank_assert, .source_display_eligible
