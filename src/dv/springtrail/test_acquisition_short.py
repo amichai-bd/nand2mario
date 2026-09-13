761e9e8bc6c06e79c0392e7bd2dc92d9758f63fe
amichaibd <amichaibd@gmail.com>
2026-09-13 07:38:39 +0300

    Register bounded acquisition CPU proofs and output fault

---
+++ src/dv/springtrail/test_acquisition_short.py
@@ -0,0 +1,8 @@
+"""Complete supplementary acquisition CPU fixture."""
+import cocotb
+import acquisition_cases
+from motion_unit_check import run
+
+@cocotb.test(timeout_time=60, timeout_unit='ms')
+async def acquisition(dut):
+    await run(dut, short=True, suite=acquisition_cases, part=None)
